96ad7b22954b0eebba58a2c761475892cb6cf65a
csw <susu199037@gmail.com>
2025-05-01 17:35:08 +0900

    tttttttt

--- src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
+++ src/test/java/sample/cafekiosk/spring/domain/stock/StockTest.java
@@ -85,4 +85,33 @@ class StockTest {
     );
   }
 
+  @DisplayName("재고 차감 시나리오2")
+  @TestFactory
+  Collection<DynamicTest> stockDeductionDynamicTest2() {
+    // given
+    Stock stock = Stock.create("001", 1);
+
+    return List.of(
+        DynamicTest.dynamicTest("재고를 주어진 개수만큼 차감할 수 있다.", () -> {
+          // given
+          int quantity = 1;
+
+          // when
+          stock.deductQuantity(quantity);
+
+          // then
+          assertThat(stock.getQuantity()).isZero();
+        }),
+        DynamicTest.dynamicTest("재고보다 많은 수의 수량으로 차감 시도하는 경우 예외가 발생한다.", () -> {
+          // given
+          int quantity = 1;
+
+          // when
+          assertThatThrownBy(() -> stock.deductQuantity(quantity))
+              .isInstanceOf(IllegalArgumentException.class)
+              .hasMessage("차감할 재고 수량이 없습니다.");
+        })
+    );
+  }
+
 }
