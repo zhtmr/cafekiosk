b682825966118dda00e039ffcbab023cbfc306b9
csw <susu199037@gmail.com>
2025-05-05 15:27:49 +0900

    상품 등록 API 문서 개선 및 RestDocs 설정 수정
    - 상품 등록 API 설명 추가: 기능 및 응답 내용 보완
    - ObjectMapper 설정 변경: ISO-8601 날짜 형식 지원
    - cleanDocs Gradle task 추가: 이전 문서 파일 삭제 자동화

--- src/test/java/sample/cafekiosk/spring/docs/RestDocsSupport.java
+++ src/test/java/sample/cafekiosk/spring/docs/RestDocsSupport.java
@@ -1,6 +1,7 @@
 package sample.cafekiosk.spring.docs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.restdocs.RestDocumentationContextProvider;
@@ -14,7 +15,9 @@ import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.docu
 public abstract class RestDocsSupport {
 
   protected MockMvc mockMvc;
-  protected ObjectMapper objectMapper = new ObjectMapper();
+  protected ObjectMapper objectMapper = new ObjectMapper()
+      .findAndRegisterModules() // Jackson의 모든 모듈을 자동으로 등록
+      .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false); // ISO-8601 형식으로 날짜 출력;
 
   @BeforeEach
   void setUp(RestDocumentationContextProvider contextProvider) {
